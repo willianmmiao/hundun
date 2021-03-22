aba7f3b659645ae56b5a049ead115b98e56a2b53
willianmmiao <76838280+willianmmiao@users.noreply.github.com>
2021-03-22 15:30:42 +0800

    Update youth.py

--- quanx/youth.py
+++ quanx/youth.py
@@ -790,9 +790,9 @@ def run():
   print(content)
 
   # 每天 23:00 发送消息推送
-  if beijing_datetime.hour == 14 and beijing_datetime.minute >= 0 and beijing_datetime.minute < 30:
+  if beijing_datetime.hour == 15 and beijing_datetime.minute >= 0 and beijing_datetime.minute < 50:
     send(title=title, content=result)
-  elif not beijing_datetime.hour == 14:
+  elif not beijing_datetime.hour == 15:
     print('未进行消息推送，原因：没到对应的推送时间点\n')
   else:
     print('未在规定的时间范围内\n')
